214a9d0a17cd06e62bc96fe770e11ceeba97aee3
Nick S <nsmith448@gmail.com>
2014-04-10 18:55:09 -0400

    Improved input generation for ipvx and time out

--- clock/src/py/generate.py
+++ clock/src/py/generate.py
@@ -2,15 +2,20 @@
 from clock import CLOCKS, SEGMENTS, powerset, mergebits, unmergeclocks
 import random
 import math
+import sys
 
-NUM_DATA_SETS = 1000
+def easy():
+    main(100, 5)
 
-def main():
-    print(NUM_DATA_SETS)
-    for K in range(1, NUM_DATA_SETS + 1):
-        N = random.randint(1, 10)
+def hard():
+    main(1000, 10)
+
+def main(datasets, maxclocks):
+    print(datasets)
+    for K in range(1, datasets + 1):
+        N = random.randint(2, maxclocks)
         print("%d %d" % (K, N))
-        clocks = random.sample(CLOCKS, N+1)
+        clocks = random.sample(CLOCKS, N)
 
         off = tuple(mergebits(random.sample(SEGMENTS, random.randint(0,2))) for i in range(4))
         displays = [unmergeclocks(clock, off) for clock in clocks]
@@ -18,4 +23,14 @@ def main():
             print(" ".join(str(d) for d in display))
 
 if __name__ == '__main__':
-    main()
+    if len(sys.argv) != 2:
+        print("missing argument")
+        sys.exit(1)
+
+    if sys.argv[1] == 'easy':
+        easy()
+    elif sys.argv[1] == 'hard':
+        hard()
+    else:
+        print("unknown argument: %s" % (sys.argv[1]))
+        sys.exit(1)
--- ipvx/src/py/generate.py
+++ ipvx/src/py/generate.py
@@ -1,12 +1,24 @@
 #!/usr/bin/env python
 import random
+import sys
 
-def main(P):
+def easy():
+    main(100, 16)
+
+def hard():
+    main(1000, 64)
+
+def main(P, maxM):
     print(P)
     for K in range(1, P+1):
-        M = random.randint(2,64)
+        M = random.randint(2,maxM)
         N = random.randint(1,M-1)
         print(K, M, N)
 
 if __name__ == '__main__':
-    main(1000)
+    if len(sys.argv) != 2:
+        print("easy or hard")
+    elif sys.argv[1] == 'easy':
+        easy()
+    elif sys.argv[1] == 'hard':
+        hard()
